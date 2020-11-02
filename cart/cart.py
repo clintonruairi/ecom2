2bc9d1a66cbe676f837e055fd4e54de0232acf82
Ruairi <clintonruairi@gmail.com>
2020-11-02 19:14:10 +0700

    discount coupon system added

--- cart/cart.py
+++ cart/cart.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from store.models import Product
-
+from coupons.models import Coupon
 
 class Cart(object):
 
@@ -13,6 +13,7 @@ class Cart(object):
             # save an empty item in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
@@ -60,6 +61,23 @@ class Cart(object):
         # remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
+    
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
         
 
 
